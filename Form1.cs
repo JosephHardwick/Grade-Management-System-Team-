2a9013947c18339da57d6338ed261b9d955336e9
Anton Hannouneh <anton_hannouneh@mymail.eku.edu>
2024-11-19 19:23:59 -0500

    TESTPUSH

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Diagnostics.Metrics;
+//using System.Runtime.Remoting.Messaging;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace CSC440Team
 {
     public partial class Form1 : Form
@@ -5,6 +12,39 @@ namespace CSC440Team
         public Form1()
         {
             InitializeComponent();
+            Console.WriteLine("Hello World");
+            MessageBox.Show("Hello World");
+            //creting the connection to the database
+            string connStr = "server=csitmariadb.eku.edu;user=student;database=csc340_db;port=3306;password=Maroon@21?;";
+
+            MySql.Data.MySqlClient.MySqlConnection conn = new MySql.Data.MySqlClient.MySqlConnection(connStr);
+
+            try
+            {
+
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                string sql = "CREATE TABLE IF NOT EXISTS AntonJoeTest" +
+             " (ID INT PRIMARY KEY AUTO_INCREMENT, " +
+
+             " Start DATETIME NOT NULL, " +
+             " End DATETIME NOT NULL, " +
+             " Description VARCHAR(1500) NULL, " +
+             "personID INT, " +
+             " Name VARCHAR(20) NOT NULL)";
+                //execute the command
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("executing");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                MessageBox.Show(ex.ToString());
+            }
+            //close the connection
+            conn.Close();
+            Console.WriteLine("Done.");
         }
     }
 }
