ba420114c3c641230c4bbb8ca544756ffac332c6
Joseph Hardwick <Josephhardwick04@gmail.com>
2024-11-21 18:41:08 -0500

    finished implementation of delete

--- DeleteGrade.Designer.cs
+++ DeleteGrade.Designer.cs
@@ -32,21 +32,28 @@
             label1 = new Label();
             button1 = new Button();
             mainViewButton = new Button();
-            ButtonPanel = new Panel();
+            listView1 = new ListView();
+            columnHeader2 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            columnHeader6 = new ColumnHeader();
+            columnHeader1 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            button2 = new Button();
+            label2 = new Label();
             SuspendLayout();
             // 
             // StudentID
             // 
-            StudentID.Location = new Point(28, 81);
+            StudentID.Location = new Point(12, 48);
             StudentID.Mask = "000000000";
             StudentID.Name = "StudentID";
-            StudentID.Size = new Size(100, 23);
+            StudentID.Size = new Size(108, 23);
             StudentID.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(28, 52);
+            label1.Location = new Point(12, 12);
             label1.Name = "label1";
             label1.Size = new Size(120, 15);
             label1.TabIndex = 1;
@@ -54,7 +61,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(134, 81);
+            button1.Location = new Point(12, 77);
             button1.Name = "button1";
             button1.Size = new Size(108, 23);
             button1.TabIndex = 8;
@@ -64,7 +71,7 @@
             // 
             // mainViewButton
             // 
-            mainViewButton.Location = new Point(28, 397);
+            mainViewButton.Location = new Point(12, 335);
             mainViewButton.Name = "mainViewButton";
             mainViewButton.Size = new Size(75, 41);
             mainViewButton.TabIndex = 20;
@@ -72,19 +79,76 @@
             mainViewButton.UseVisualStyleBackColor = true;
             mainViewButton.Click += mainViewButton_Click;
             // 
-            // ButtonPanel
+            // listView1
             // 
-            ButtonPanel.Location = new Point(326, 52);
-            ButtonPanel.Name = "ButtonPanel";
-            ButtonPanel.Size = new Size(462, 386);
-            ButtonPanel.TabIndex = 21;
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader2, columnHeader5, columnHeader6, columnHeader1, columnHeader3 });
+            listView1.Location = new Point(138, 12);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(403, 321);
+            listView1.TabIndex = 21;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.DisplayIndex = 1;
+            columnHeader2.Text = "Course";
+            columnHeader2.Width = 100;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.DisplayIndex = 2;
+            columnHeader5.Text = "Semester";
+            columnHeader5.TextAlign = HorizontalAlignment.Center;
+            columnHeader5.Width = 100;
+            // 
+            // columnHeader6
+            // 
+            columnHeader6.DisplayIndex = 3;
+            columnHeader6.Text = "Grade";
+            columnHeader6.TextAlign = HorizontalAlignment.Center;
+            columnHeader6.Width = 100;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.DisplayIndex = 0;
+            columnHeader1.Text = "Name";
+            columnHeader1.TextAlign = HorizontalAlignment.Center;
+            columnHeader1.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "CRN";
+            columnHeader3.TextAlign = HorizontalAlignment.Center;
+            columnHeader3.Width = 100;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(429, 340);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 23);
+            button2.TabIndex = 22;
+            button2.Text = "Delete Grade";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(19, 134);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 23;
+            label2.Text = "label2";
             // 
             // DeleteGrade
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(ButtonPanel);
+            ClientSize = new Size(556, 388);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(listView1);
             Controls.Add(mainViewButton);
             Controls.Add(button1);
             Controls.Add(label1);
@@ -101,6 +165,13 @@
         private Label label1;
         private Button button1;
         private Button mainViewButton;
-        private Panel ButtonPanel;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+        private ColumnHeader columnHeader3;
+        private Button button2;
+        private Label label2;
     }
 }
--- DeleteGrade.cs
+++ DeleteGrade.cs
@@ -13,78 +13,77 @@ namespace CSC440Team
 {
     public partial class DeleteGrade : Form
     {
+        private int studentID;
         public DeleteGrade()
         {
             InitializeComponent();
             //Student student = new Student(int.Parse(StudentID.Text));
+            label2.Enabled = false;
+
 
         }
+        private void populateList(string cName,string course, string semester, string grade, string crn)
+        {
 
+
+            ListViewItem item = new ListViewItem(course);
+            item.SubItems.Add(semester);
+            item.SubItems.Add(grade);
+            item.SubItems.Add(cName);
+            item.SubItems.Add(crn);
+            listView1.Items.Add(item);
+
+
+
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            //compress the string in the masked text box to an int
+            listView1.Items.Clear();
+            // Compress the string in the masked text box to an int
             string compressedID = StudentID.Text.Replace(" ", "");
+            studentID = int.Parse(compressedID);
 
             Student student = new Student(int.Parse(compressedID));
             if (!student.exists())
             {
                 MessageBox.Show("Student does not exist");
+                return;
             }
 
-
-            //display all grades in selectable objects
-            //if selected and confirmed, delete the grade, run student.updateGPA()
+            // Display all grades in selectable objects
+            // If selected and confirmed, delete the grade, run student.updateGPA()
             string connStr = "server=csitmariadb.eku.edu;user=student;database=csc340_db;port=3306;password=Maroon@21?;";
-            string query = "SELECT* FROM cabj_grades_1 WHERE StudentID = @ID";
-            MySql.Data.MySqlClient.MySqlConnection conn = new MySql.Data.MySqlClient.MySqlConnection(connStr);
-
+            string query = "SELECT * FROM cabj_grades_1 WHERE StudentID = @ID";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            string name = "";
             try
             {
-
                 Console.WriteLine("Connecting to MySQL...");
                 conn.Open();
-                //string sql = query;
-                //execute the command
 
                 MySqlCommand cmd = new MySqlCommand(query, conn);
                 cmd.Parameters.AddWithValue("@ID", int.Parse(compressedID));
-                //cmd.ExecuteNonQuery();
-                //MessageBox.Show("executing");
                 MySqlDataReader reader = cmd.ExecuteReader();
+                if (!reader.HasRows)
+                {
+                    MessageBox.Show("Student Does Not Have Any Grades To Delete.");
+                    return;
+                }
                 while (reader.Read())
                 {
-                    if (!reader.HasRows)
-                    {
-                        MessageBox.Show("Student Does Not Have Any Grades To Delete.");
-                        return;
-                    }
+                    
                     int CRN = reader.GetInt32(reader.GetOrdinal("CRN"));
                     char grade = reader.GetChar(reader.GetOrdinal("Grade"));
-                    //get the hours for that course
-                    Course course = new Course(CRN);//use the CRN constructor
+                    Course course = new Course(CRN); // Use the CRN constructor
                     int hours = course.Hours;
                     string prefix = course.Prefix;
                     string number = course.Number;
                     string semester = course.Semester;
                     int year = course.Year;
-                    string name = student.Name;
-                    string cName = course.Name;//course name
-                    
-                    int buttonSpacing = 10; // Set the vertical spacing between buttons
-                    int buttonHeight = 100; // Set the height of each button
+                    name = student.Name;
+                    string cName = course.Name; // Course name
 
-                    Button button = new Button
-                    {
-                        Text = name + "\n " + 
-                        "Course: " + cName + "\n" +"Hours: " + hours + " " + "\n"
-                        + prefix + " " + number + " " + semester +
-                        " " + year + "\n " + "Grade: " + grade,
-                        Location = new Point(10, 10 + (buttonHeight + buttonSpacing) * this.ButtonPanel.Controls.Count),
-                        Size = new Size(200, buttonHeight)
-                    };
-                    
-                    
-                    
+                    populateList(cName, prefix + " " + number, semester + " " + year, grade.ToString(), CRN.ToString());
                 }
             }
             catch (Exception ex)
@@ -92,13 +91,13 @@ namespace CSC440Team
                 Console.WriteLine(ex.ToString());
                 MessageBox.Show(ex.ToString());
             }
-            //close the connection
+            label2.Enabled = true;
+            label2.Text = "Deleting Grade For: \n" + name ;
+            MessageBox.Show(name);
+            // Close the connection
             conn.Close();
-
-
-
-
         }
+
 
         private void mainViewButton_Click(object sender, EventArgs e)
         {
@@ -106,6 +105,35 @@ namespace CSC440Team
             Form1 form1 = new Form1();
             this.Hide();
             form1.Show();
+        }
+        //delete button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string connStr = "server=csitmariadb.eku.edu;user=student;database=csc340_db;port=3306;password=Maroon@21?;";
+            string query = "DELETE FROM cabj_grades_1 WHERE StudentID = @ID AND CRN = @CRN";
+            MySql.Data.MySqlClient.MySqlConnection conn = new MySql.Data.MySqlClient.MySqlConnection(connStr);
+
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                string CRN = listView1.SelectedItems[0].SubItems[4].Text;
+                cmd.Parameters.AddWithValue("@ID", studentID);
+                cmd.Parameters.AddWithValue("@CRN", CRN);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                MessageBox.Show(ex.ToString());
+            }
+            conn.Close();
+            button1_Click(this, EventArgs.Empty); // Call button1_Click method programmatically
+            Student student = new Student(studentID);
+            student.updateGPA();
+
         }
     }
 }
